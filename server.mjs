4370fb4a864d00594a07ac0ec1f0d360d0da6ded
akoonth1 <a.koontharana@gmail.com>
2024-11-19 11:23:30 -0500

    Starting the backend of Capstone

---
+++ server.mjs
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import connectToDatabase from "./config/db.mjs";
+// import router from "./routes/route.mjs";
+import { Schema } from "mongoose";
+
+
+import cors from "cors";
+import morgan from "morgan"; // middleware for logging requests
+
+
+
+const app = express();
+dotenv.config();
+
+connectToDatabase();
+
+let PORT= process.env.PORT || 3000;  // default port to listen
+
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+// app.use("/", router);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
