90a4670bf6df4bb84f96f4c28948db8249baeb6f
akoonth1 <a.koontharana@gmail.com>
2024-11-25 22:34:35 -0500

    added readme

--- models/UsersSchema.mjs
+++ models/UsersSchema.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: false },
+    name: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, index: true, unique: true },
     createdAt: { type: Date, default: Date.now },
